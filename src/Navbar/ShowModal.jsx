c98197780fdf2115dfe9b87ab5c9adeaee37c3a9
AJ5154 <ashishjadhav597@gmail.com>
2023-06-07 13:03:40 +0530

    commit message

--- src/Navbar/ShowModal.jsx
+++ src/Navbar/ShowModal.jsx
@@ -1,28 +1,37 @@
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./ShowModal.css";
 
 const MyModal = ({ closeModal, setUser }) => {
   const username = useRef();
   const password = useRef();
+
   function handleLogin() {
-    if (
-      username.current.value === "ashish" &&
-      password.current.value === "123"
-    ) {
-      setUser("Ashish");
+    const enteredUsername = username.current.value;
+    const enteredPassword = password.current.value;
+
+    if (enteredUsername && enteredPassword) {
+      // Capitalize the first letter of the username
+      const capitalizedUsername =
+        enteredUsername.charAt(0).toUpperCase() + enteredUsername.slice(1);
+
+      // Set the capitalized username as the logged-in user
+      setUser(capitalizedUsername);
+    } else {
+      alert("Please enter a username and password");
     }
 
     closeModal();
   }
+
   return (
     <>
       <div className="modal-wrapper" onClick={closeModal}></div>
       <div className="modal-container">
         <h2 className="modal-h2">Log In</h2>
         <p className="modal-p">
-          By continuing, you agree are setting up a Reddit account and agree to
-          our <a href="https://example.com">User Agreement</a> and{" "}
-          <a href="https://example.com">Privacy Policy</a> Privacy Policy.
+          By continuing, you agree to set up a Reddit account and abide by our{" "}
+          <a href="https://example.com">User Agreement</a> and{" "}
+          <a href="https://example.com">Privacy Policy</a>.
         </p>
         <input
           ref={username}
@@ -38,11 +47,12 @@ const MyModal = ({ closeModal, setUser }) => {
           placeholder="Password"
         />
         <br />
-        <button type="buttton" className="modal-btn" onClick={handleLogin}>
+        <button type="button" className="modal-btn" onClick={handleLogin}>
           Log In
         </button>
       </div>
     </>
   );
 };
+
 export default MyModal;
