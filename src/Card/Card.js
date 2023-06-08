0e537ac5275aafbea816540fa2906f64919c1d73
AJ5154 <ashishjadhav597@gmail.com>
2023-06-09 00:50:37 +0530

    commit message

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
+import axios from "axios";
 export const Card = () => {
+  const [data, setData] = useState([]);
+  const [newNews, setNewNews] = useState([]);
+  const [topNews, setTopNews] = useState([]);
+  const getHotNews = () => {
+    axios
+      .get(
+        " https://newsapi.org/v2/top-headlines?country=us&apiKey=eb72608b2f62483d83980969c871b0b3"
+      )
+      .then((response) => {
+        setData(response.data.articles);
+      });
+  };
+  const getNewNews = () => {
+    axios
+      .get(
+        " https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=eb72608b2f62483d83980969c871b0b3"
+      )
+      .then((response) => {
+        setNewNews(response.data.articles);
+      });
+  };
+  const getTopNews = () => {
+    axios
+      .get(
+        " https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=eb72608b2f62483d83980969c871b0b3"
+      )
+      .then((response) => {
+        setTopNews(response.data.articles);
+      });
+  };
   return (
     <>
       <div className="card-main">
@@ -10,198 +41,198 @@ export const Card = () => {
           <div className="card-popular-post-body">
             <ul>
               <li>
-                <button type="button" className="card-btn">
+                <button type="button" class="card-btn" onClick={getHotNews}>
                   <i className="bi-fire">
                     <span>Hot</span>
                   </i>
                 </button>
               </li>
               <li>
-                <div className="dropdown">
+                <div class="dropdown">
                   <button
-                    className="card-btn dropdown-toggle"
+                    class="card-btn dropdown-toggle"
                     type="button"
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                   >
                     India
-                    <ul className="dropdown-menu">
+                    <ul class="dropdown-menu">
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Everywhere
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           United States
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Argentina
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Australia
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Bulgaria
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Canada
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Chile
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Colombia
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Croatia
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Finland
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           France
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Germany
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Greece
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Hungary
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Iceland
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           India
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Ireland
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Italy
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Japan
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Malaysia
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Mexico
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           New Zealand
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Poland
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Portugal
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Romania
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Serbia
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Singapore
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Spain
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Sweden
                         </a>
                       </li>
                       <br />
                       <li>
-                        <a className="dropdown-item" href="https://example.com">
+                        <a class="dropdown-item" href="#">
                           Taiwan
                         </a>
                       </li>
@@ -211,14 +242,14 @@ export const Card = () => {
                 </div>
               </li>
               <li>
-                <button type="button" className="card-btn">
+                <button type="button" class="card-btn" onClick={getNewNews}>
                   <i className="bi-plus-circle-dotted">
                     <span>New</span>
                   </i>
                 </button>
               </li>
               <li>
-                <button type="button" className="card-btn">
+                <button type="button" class="card-btn" onClick={getTopNews}>
                   <i className="bi-bar-chart">
                     <span>Top</span>
                   </i>
@@ -229,83 +260,39 @@ export const Card = () => {
         </div>
 
         {/* 1 */}
-        <div className="card">
-          <div className="card-body">
-            <div className="icons">
-              <i className="bi-arrow-up-circle"></i>
-              <br />
-              <i className="bi-arrow-down-circle"></i>
+        {data.map((value) => {
+          return (
+            <div className="card">
+              <img src={value.urlToImage} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">{value.title}</h4>
+                <p className="card-text">{value.description}</p>
+              </div>
             </div>
-            <div className="card-matter">
-              <h3 className="card-title">
-                BCCI mulls Diwali window for the WPL
-              </h3>
-
-              <img
-                className="card-img"
-                src="https://www.cricbuzz.com/a/img/v1/595x396/i1/c287469/the-inaugural-season-was-held.jpg"
-                alt="WPL"
-              />
-              <p className="card-text">
-                The Board of Control for Cricket in India (BCCI) is
-                contemplating having a Diwali window for the Women's Premier
-                League (WPL), the maiden edition of which ended last month. The
-                festival season for the league means it could clash with the
-                home international matches but the BCCI is wanting to have a gap
-                between the IPL and WPL. Five days after WPL concluded on March
-                26 this year, the IPL had started. The BCCI also will introduce
-                the IPL's home and away format which could not be held this year
-                due to want of time. "We are mulling the possibility of
-                scheduling the WPL, in the home and away format, in the Diwali
-                window; not two seasons in a year but just a different time
-                window," Jay Shah said on Friday.
-              </p>
+          );
+        })}
+        {newNews.map((value) => {
+          return (
+            <div className="card">
+              <img src={value.urlToImage} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">{value.title}</h4>
+                <p className="card-text">{value.description}</p>
+              </div>
             </div>
-          </div>
-        </div>
-        {/* 2 */}
-        <div className="card">
-          <div className="card-body">
-            <div className="icons">
-              <i className="bi-arrow-up-circle"></i>
-              <br />
-              <i className="bi-arrow-down-circle"></i>
+          );
+        })}
+        {topNews.map((value) => {
+          return (
+            <div className="card">
+              <img src={value.urlToImage} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">{value.title}</h4>
+                <p className="card-text">{value.description}</p>
+              </div>
             </div>
-            <div className="card-matter">
-              <h3 className="card-title">
-                Fifty for Rinku Singh!{" "}
-                <a href="https://example.com">#IPL2023 #KKRvSRH</a>
-              </h3>
-              <img
-                className="card-img"
-                src="https://pbs.twimg.com/media/FtsVzm6akAAP1zb?format=jpg&name=small"
-                alt="WPL"
-              />
-            </div>
-          </div>
-        </div>
-        {/* 3 */}
-        <div className="card">
-          <div className="card-body">
-            <div className="icons">
-              <i className="bi-arrow-up-circle"></i>
-              <br />
-              <i className="bi-arrow-down-circle"></i>
-            </div>
-            <div className="card-matter">
-              <h3 className="card-title">
-                शराब घोटाले के असली रचनाकार{" "}
-                <a href="https://example.com">#ArvindKejriwal</a> का तिहाड़ जेल
-                में बंद शराब घोटाले के आरोपियों ने नाम लिया है।
-              </h3>
-              <img
-                className="card-img"
-                src="https://pbs.twimg.com/media/FtrLGbnXoAUtkEX?format=jpg&name=900x900"
-                alt="WPL"
-              />
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
